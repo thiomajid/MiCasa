d193213c7ac22b54a9ee13318a4bb27200413eec
Thiombiano Abdoul Majid <82466066+ATOM36@users.noreply.github.com>
2022-09-18 18:39:20 +0100

    Update environment.prod.ts

--- MiCasa/src/environments/environment.prod.ts
+++ MiCasa/src/environments/environment.prod.ts
@@ -1,20 +1,20 @@
 export const environment = {
   firebase: {
-    projectId: 'micasa-project-3e207',
-    appId: '1:194476213453:web:b5e637f2069a2835f41584',
-    storageBucket: 'micasa-project-3e207.appspot.com',
-    locationId: 'europe-west',
-    apiKey: 'AIzaSyDkRtTUxZUUb38rcWvhYWIZp7b2hV9KgTI',
-    authDomain: 'micasa-project-3e207.firebaseapp.com',
-    messagingSenderId: '194476213453',
-    measurementId: 'G-TLLYF0SZGN',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    locationId: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
+    measurementId: '',
   },
 
   production: true,
   apiUrl: 'http://localhost:5000/api/',
 
   openWeather: {
-    apiUrl: 'https://api.openweathermap.org/data/2.5/',
-    apiKey: 'a77aa463da3599204abbaf825e980b26',
+    apiUrl: '',
+    apiKey: '',
   },
 };
